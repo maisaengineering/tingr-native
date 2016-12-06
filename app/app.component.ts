90f267256273c8e8b7fff9d7322c17b19b8bea8f
Upender Devulapally <upender.devulapally@gmail.com>
2016-12-06 15:51:19 +0530

    Login page for entering email and binding the value

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,15 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "my-app",
-  template: "<Label text='hello world'></Label>"
+  templateUrl: "pages/login/login.html",
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  isLoggingIn = false;
+  email = "";
+
+  submitEmail() {
+    alert("You’re using: " + this.email);
+  }
+
+}
