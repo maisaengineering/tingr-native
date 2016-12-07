cbff9f9c39b52b696abee25680fea97811c08b40
Upender Devulapally <upender.devulapally@gmail.com>
2016-12-07 12:24:02 +0530

    UI: login component cleanuo

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -1,37 +1,27 @@
-import { Color } from "color";
-import { View } from "ui/core/view";
 import { Router } from "@angular/router";
-import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
-
-import { Teacher } from "../../shared/teacher/teacher";
-import { TeacherService } from "../../shared/teacher/teacher.service";
+import { Component, OnInit } from "@angular/core";
 import { Page } from "ui/page";
 
 @Component({
     selector: "my-app",
-    providers: [TeacherService],
+    providers: [],
     templateUrl: "pages/login/login.html",
     styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 export class LoginComponent implements OnInit {
-    teacher: Teacher;
-    isLoggingIn = true;
-    @ViewChild("container") container: ElementRef;
+    isLoggingIn = false;
+    email = '';
 
     ngOnInit() {
         this.page.actionBarHidden = true;
-       // this.page.backgroundImage = "res://bg_login";
     }
 
-
-    constructor(private router: Router, private teacherService: TeacherService, private page: Page) {
-        this.teacher = new Teacher();
-        this.teacher.email = "user@nativescript.org";
-        this.teacher.password = "password";
+    constructor(private router: Router, private page: Page) {
+        this.email = "user@nativescript.org";
     }
 
     submitEmail() {
-        console.log("Email: " + this.teacher.email);
+        console.log("Email: " + this.email);
         this.router.navigate(["/verify-password"]);
     }
 
